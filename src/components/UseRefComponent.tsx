8c60c6c9c99b02979a462be44a7a2556b155e3be
jaboulos <jab12288@gmail.com>
2021-04-06 10:23:30 -0700

    Add input text display

--- src/components/UseRefComponent.tsx
+++ src/components/UseRefComponent.tsx
@@ -1,9 +1,23 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 const UseRefComponent = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const [text, setText] = useState<string | null>(null);
+  return (
+    <div>
+      <div>
+        <p>
+          Input Text: <strong>{text}</strong>
+        </p>
+      </div>
 
-  return <input placeholder='Hellloooo world' ref={inputRef} />;
+      <input
+        placeholder='Hellloooo world'
+        ref={inputRef}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default UseRefComponent;
